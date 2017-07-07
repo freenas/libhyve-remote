948891611dba3891a243c69a8ccc2641dae5ce83
Marcelo Araujo <araujo@FreeBSD.org>
2017-07-07 15:38:42 +0800

    Define libhyverem path in the test file.

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <string.h>
 
+#define LIBHYVEREM  "/usr/local/lib/libhyverem.so"
 int main(void);
 
 int
@@ -17,7 +18,7 @@ main(void) {
     int (*load_functions_a)(void);
 
     loader = NULL;
-    loader = strdup("/z/github/libhyverem/libhyverem.so");
+    loader = strdup(LIBHYVEREM);
     shlib = dlopen(loader, RTLD_NOW);
 
     if (!shlib) {
@@ -30,13 +31,8 @@ main(void) {
     check_sharedlibs = dlsym(shlib, "check_sharedlibs");
     load_functions_a = dlsym(shlib, "load_functions");
 
-    int l = 0;
-    load_functions_a();
-
-    /*
-    int l = load_functions();
+    int l = load_functions_a();
     printf("Load functions: %d\n", l);
-    */
 
     int check;
     check = check_sharedlibs("/usr/local/lib/libvncserver.so");
